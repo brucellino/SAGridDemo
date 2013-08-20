2404f3b0157f31e22ff3115dd4030f17d2960846
brucellino <brucellino@gmail.com>
2013-08-21 01:02:43 +0200

    first update to install cmake script

--- standalone/pythia/install-cmake.sh
+++ standalone/pythia/install-cmake.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 # optional cmake installation script should cmake not be available
+
+# check that cmake is on the system
+if [ `which cmake` ] ; then
+    echo "Cmake is already installed on this machine, exiting"
+    exit 0;
+fi
+
+# if not, download it and install it to a working directory
+CMAKE_VERSION=2.8.11.2
+CMAKE_TARBALL=cmake-${CMAKE_VERSION}.tar.gz 
+CMAKE_URL=http://www.cmake.org/files/v2.8/cmake-${CMAKE_VERSION}.tar.gz
+
+wget ${CMAKE_URL}
+tar xvfz ${CMAKE_TARBALL}
+cd cmake-${CMAKE_VERSION}
+./bootstrap
+make
+
